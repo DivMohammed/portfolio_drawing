c42ac96b3791057f12ba04181ad3ab5f787ad641
liix5 <layan14243@gmail.com>
2024-01-15 23:32:41 +0300

    navbar

--- main/js/animation.js
+++ main/js/animation.js
@@ -1,5 +1,8 @@
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(ScrollToPlugin)
+let x = document.getElementById("nav");
+let links = document.querySelectorAll('a');
+
 
 //header animation
 let headerTl = gsap.timeline();
@@ -30,7 +33,7 @@ let headerTl = gsap.timeline();
         .from('.hold_home_text p',{x:'200px', opacity:0,duration:1}, '<' )
         .from('.hold_home_text button',{x:'-200px', opacity:0,duration:1}, '<' )
 
-
+//about animation
 let aboutTl = gsap.timeline({scrollTrigger:{
   trigger:'.hold_info',
   start:'top center',
@@ -43,49 +46,88 @@ aboutTl.from('.hold_info_imgs',{x:'-200px', opacity:0,duration:1.4})
        .from('.hold_info_text p',{x:'200px', opacity:0,duration:1},'<0.5')
        .from('.hold_info_text button',{x:'200px', opacity:0,duration:1},'<0.5')
 
-let arttl = gsap.timeline({scrollTrigger:{
-  trigger:'.art',
-  start:'top center',
-  end:'bottom center',
+
+
+//artwork animation
+//responsive animation
+
+ScrollTrigger.matchMedia({
+	
+	// desktop
+	"(min-width: 1200px)": function() {
+    let arttl = gsap.timeline({scrollTrigger:{
+      trigger:'.art',
+      start:'top center',
+      end:'bottom center',
+      toggleActions: 'restart reverse restart reverse'
+    },});
+    
+    arttl.fromTo(".card",{x:'-2000px', rotation:20},{ x:0,
+      rotation:0,
+      duration:1.5,
+      ease:'back(0.6)',
+      stagger:{
+        amount:0.9,
+        from:'end'
+      }})
+      .from('.art >p', {
+        y:'-100px',
+        opacity:0,
+        duration:2,
+        scale:0.5
+      } ,'<')
+      .from('.art >hr', {
+        scale:0,
+        opacity:0, duration:1.5,
+        ease:'back'
+        }, '<0.2')
+  }, 
   
-  //it can be play pause resume reverse restart reset complete none  
-  toggleActions: 'restart reverse restart reverse'
-},});
+	// mobile
+	"(max-width: 1200px)": function() {
+		
+    let arttl = gsap.timeline({scrollTrigger:{
+      trigger:'.art',
+      start:'top center',
+      end:'bottom center',
+      toggleActions: 'restart reverse complete reverse'
+    },});
+    
+    arttl.fromTo(".card",{x:'-2000px', rotation:20},{ x:0,
+      rotation:0,
+      duration:1.5,
+      ease:'back(0.6)',
+      stagger:0.9})
+      .from('.art >p', {
+        y:'-100px',
+        opacity:0,
+        duration:2,
+        scale:0.5
+      } ,'<')
+      .from('.art >hr', {
+        scale:0,
+        opacity:0, duration:1.5,
+        ease:'back'
+        }, '<0.2')
+  }, 
+	
+  
+});
 
-arttl.fromTo(".card",{x:'-2000px', rotation:20},{ x:0,
-  rotation:0,
-  duration:1.5,
-  ease:'back(0.6)',
-  stagger:{
-    amount:0.9,
-    from:'end'
-  }})
-  .from('.art >p', {
-    y:'-100px',
-    opacity:0,
-    duration:2,
-    scale:0.5
-  } ,'<')
-  .from('.art >hr', {
-    scale:0,
-    opacity:0, duration:1.5,
-    ease:'back'
-    }, '<0.2')
 
 
+//contact animation
 contactTl= gsap.timeline({scrollTrigger:{
   trigger:'.contact',
   start:'top center',
   end:'bottom center',
-  
-  //it can be play pause resume reverse restart reset complete none  
-  toggleActions: 'restart reverse restart reverse'
+ toggleActions: 'restart reverse restart reverse'
 },});
 contactTl.from('.contact >p', {y:'-100px',opacity:0,duration:1.5,scale:0.5} )
        .from('.contact >hr', {scale:0,opacity:0, duration:1.5,ease:'back'}, '<0.2')
        .from(".contact h3", {opacity:0, x:'-80px', ease:'back' }, 0.1)
        .from(".form div", {opacity:0, x:'-80px', stagger:0.2 }, '<0.3')
-       .from(".form button", {opacity:0, x:'-80px' },'<0.6')
+       .from(".form button", {opacity:0, x:'-80px' },'<0.6');
 
 
 
@@ -93,10 +135,13 @@ contactTl.from('.contact >p', {y:'-100px',opacity:0,duration:1.5,scale:0.5} )
       
 window.addEventListener('resize',()=> ScrollTrigger.refresh( ))
 
-let links = document.querySelectorAll('a')
+//navbar 
+
 links.forEach((a, index)=>{
   a.addEventListener('click',()=>{
-    
+    //make the nav bar disapear onclick
+    x.className = "navBar";
+    //animation
     gsap.to(window, {duration: 1, scrollTo:"#section" + (index + 1)});
   })
 })
@@ -112,3 +157,18 @@ button2.addEventListener('click',()=>{
     
   gsap.to(window, {duration: 1, scrollTo:"#section4"});
 })
+
+
+
+
+function navBar() {
+  
+  
+  console.log(x.className)
+  if (x.className === "navBar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navBar";
+  }
+}
+

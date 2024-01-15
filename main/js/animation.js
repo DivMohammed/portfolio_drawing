73ae2e5627355c8cec4246dccbc016ac2f811452
liix5 <layan14243@gmail.com>
2024-01-15 18:56:27 +0300

    animation

--- main/js/animation.js
+++ main/js/animation.js
@@ -1,6 +1,7 @@
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(ScrollToPlugin)
 
+//header animation
 let headerTl = gsap.timeline();
   headerTl.from('header a', {
     opacity:0,
@@ -17,19 +18,41 @@ let headerTl = gsap.timeline();
     ease:'back(0.8)'
   },'<0.7' )
   
+  //home animation
+  let homeTl= gsap.timeline({scrollTrigger:{
+    trigger:'.hold_home',
+    start:'top center',
+    end:'bottom center',
+    toggleActions: 'restart reverse restart reverse'
+  },});
+
+  homeTl.from('.hold_home_text h1',{x:'-200px', opacity:0,duration:1} )
+        .from('.hold_home_text p',{x:'200px', opacity:0,duration:1}, '<' )
+        .from('.hold_home_text button',{x:'-200px', opacity:0,duration:1}, '<' )
 
 
+let aboutTl = gsap.timeline({scrollTrigger:{
+  trigger:'.hold_info',
+  start:'top center',
+  end:'bottom center',
+  toggleActions: 'restart reverse restart reverse'
+},});
 
-let tl = gsap.timeline({scrollTrigger:{
+aboutTl.from('.hold_info_imgs',{x:'-200px', opacity:0,duration:1.4})
+       .from('.hold_info_text h2',{x:'200px', opacity:0,duration:1},'<')
+       .from('.hold_info_text p',{x:'200px', opacity:0,duration:1},'<0.5')
+       .from('.hold_info_text button',{x:'200px', opacity:0,duration:1},'<0.5')
+
+let arttl = gsap.timeline({scrollTrigger:{
   trigger:'.art',
   start:'top center',
   end:'bottom center',
-  markers:true,
+  
   //it can be play pause resume reverse restart reset complete none  
   toggleActions: 'restart reverse restart reverse'
 },});
 
-tl.fromTo(".card",{x:'-2000px', rotation:20},{ x:0,
+arttl.fromTo(".card",{x:'-2000px', rotation:20},{ x:0,
   rotation:0,
   duration:1.5,
   ease:'back(0.6)',
@@ -54,19 +77,18 @@ contactTl= gsap.timeline({scrollTrigger:{
   trigger:'.contact',
   start:'top center',
   end:'bottom center',
-  markers:true,
+  
   //it can be play pause resume reverse restart reset complete none  
   toggleActions: 'restart reverse restart reverse'
 },});
 contactTl.from('.contact >p', {y:'-100px',opacity:0,duration:1.5,scale:0.5} )
-.from('.contact >hr', {
-  scale:0,
-  opacity:0, duration:1.5,
-  ease:'back'
-  }, '<0.2')
+       .from('.contact >hr', {scale:0,opacity:0, duration:1.5,ease:'back'}, '<0.2')
        .from(".contact h3", {opacity:0, x:'-80px', ease:'back' }, 0.1)
        .from(".form div", {opacity:0, x:'-80px', stagger:0.2 }, '<0.3')
        .from(".form button", {opacity:0, x:'-80px' },'<0.6')
+
+
+
 
       
 window.addEventListener('resize',()=> ScrollTrigger.refresh( ))
@@ -74,7 +96,19 @@ window.addEventListener('resize',()=> ScrollTrigger.refresh( ))
 let links = document.querySelectorAll('a')
 links.forEach((a, index)=>{
   a.addEventListener('click',()=>{
-    console.log(a.value)
+    
     gsap.to(window, {duration: 1, scrollTo:"#section" + (index + 1)});
   })
+})
+
+let button1 = document.getElementById('button1')
+let button2 = document.getElementById('button2')
+
+button1.addEventListener('click',()=>{
+    
+  gsap.to(window, {duration: 1, scrollTo:"#section2"});
+})
+button2.addEventListener('click',()=>{
+    
+  gsap.to(window, {duration: 1, scrollTo:"#section4"});
 })

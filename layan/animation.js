c1ac23ba3ac3b2bf3b2dc4a42920660e3443a2b4
liix5 <layan14243@gmail.com>
2024-01-15 14:35:08 +0300

    updated web

--- layan/animation.js
+++ layan/animation.js
@@ -1,22 +1,22 @@
 gsap.registerPlugin(ScrollTrigger);
-
+gsap.registerPlugin(ScrollToPlugin)
 
 let headerTl = gsap.timeline();
-  headerTl
-  .from('header img', {
-    y:'-30px',
+  headerTl.from('header a', {
     opacity:0,
+    y:'-80px',
+    stagger:{
+      amount:0.5,
+      from:'end'
+    },
     ease:'back(0.8)'
   })
-  .from('header a', {
-     opacity:0,
-     y:'-80px',
-     stagger:{
-       amount:0.5,
-       from:'center'
-     },
-     ease:'back(0.8)'
-   })
+  .from('header img', {
+    y:'-80px',
+    opacity:0,
+    ease:'back(0.8)'
+  },'<0.7' )
+  
 
 
 
@@ -24,7 +24,7 @@ let tl = gsap.timeline({scrollTrigger:{
   trigger:'.art',
   start:'top center',
   end:'bottom center',
-  
+  markers:true,
   //it can be play pause resume reverse restart reset complete none  
   toggleActions: 'restart reverse restart reverse'
 },});
@@ -54,18 +54,27 @@ contactTl= gsap.timeline({scrollTrigger:{
   trigger:'.contact',
   start:'top center',
   end:'bottom center',
-  
+  markers:true,
   //it can be play pause resume reverse restart reset complete none  
   toggleActions: 'restart reverse restart reverse'
 },});
-contactTl.from('.contact >p', {y:'-100px',opacity:0,duration:2,scale:0.5} ,'<')
+contactTl.from('.contact >p', {y:'-100px',opacity:0,duration:1.5,scale:0.5} )
 .from('.contact >hr', {
   scale:0,
   opacity:0, duration:1.5,
   ease:'back'
   }, '<0.2')
-       .from(".form h3", {opacity:0, x:'-100px' },0)
-       .from(".form div", {opacity:0, x:'-100px', stagger:0.2 }, '<0.3')
-       .from(".form button", {opacity:0, x:'-100px' },'<0.5')
-       .from('.social p', {opacity:0, x:'100px' } , 0)
-       .from('.social img', {opacity:0, x:'100px', stagger:0.5 }, '<0.3')
+       .from(".contact h3", {opacity:0, x:'-80px', ease:'back' }, 0.1)
+       .from(".form div", {opacity:0, x:'-80px', stagger:0.2 }, '<0.3')
+       .from(".form button", {opacity:0, x:'-80px' },'<0.6')
+
+      
+window.addEventListener('resize',()=> ScrollTrigger.refresh( ))
+
+let links = document.querySelectorAll('a')
+links.forEach((a, index)=>{
+  a.addEventListener('click',()=>{
+    console.log(a.value)
+    gsap.to(window, {duration: 1, scrollTo:"#section" + (index + 1)});
+  })
+})
